b2fb8b747687eb59b1c16ed9b90eddd0488c2bc9
Trenton Large <trentonlarge@gmail.com>
2018-11-05 00:13:44 -0800

    Update Page.vue
    remove "edit" temporarily

--- packages/@vuepress/theme-default/components/Page.vue
+++ packages/@vuepress/theme-default/components/Page.vue
@@ -160,7 +160,7 @@ export default {
 
       return (
         base.replace(endingSlashRE, '') +
-        `/edit/${docsBranch}` +
+        `/${docsBranch}` +
         (docsDir ? '/' + docsDir.replace(endingSlashRE, '') : '') +
         path
       )
